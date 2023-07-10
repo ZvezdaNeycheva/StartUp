6ad168c74b63107697e260fb53edbda56d8cc874
ZvezdaNeycheva <138030249+ZvezdaNeycheva@users.noreply.github.com>
2023-07-10 21:03:53 +0300

    Update main.js
    "Changed the language"

--- first-site-proteins/scripts/main.js
+++ first-site-proteins/scripts/main.js
@@ -6,5 +6,5 @@ function myFunction() {
  function calculateDailyProteinIntake() {
    let kg =  document.getElementById("proteinIntake").value 
    let result = Number(kg) * 1.6  
-   document.getElementById("returnProteinIntake").innerHTML = 'Your daily intake should be: '+ result.toFixed(2) +" grams"
+   document.getElementById("returnProteinIntake").innerHTML = 'Дневно трябва да приемате по:  '+ result.toFixed(2) +" грама"
  }
