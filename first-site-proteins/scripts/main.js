3e9cace186793ca62a7f9b27ab0db831b8c16f6e
ZvezdaNeycheva <138030249+ZvezdaNeycheva@users.noreply.github.com>
2023-07-10 20:56:18 +0300

    Update main.js
    "Added daily protein intake"

--- first-site-proteins/scripts/main.js
+++ first-site-proteins/scripts/main.js
@@ -1,4 +1,10 @@
 function myFunction() {
-    var element = document.body;
+    let element = document.body;
     element.classList.toggle("dark-mode");
+ }
+
+ function calculateDailyProteinIntake() {
+   let kg =  document.getElementById("proteinIntake").value 
+   let result = Number(kg) * 1.6  
+   document.getElementById("returnProteinIntake").innerHTML = 'Your daily intake should be: '+ result.toFixed(2) +" grams"
  }
